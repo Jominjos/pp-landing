f25aa5c5725d3c46a6b0b009d02e9a872187993d
jominjos <sjominjos@gmail.com>
2024-01-14 00:10:32 +0530

    muthaledu added

---
+++ src/components/textu.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "../styles/textu.css"
+const Textu = () => {
+  return (
+    <div  className="container">
+    <div id='center_div' className="row">
+      <div id='textu' className="col-md-6 text-center">
+        <h3 id='muthal' className="animate-charcter"> முதலீடு</h3>
+        <h1 className="animate-charcter">10,00,00,000</h1>
+        <h3 id='muthal' className="animate-charcter"> லாபம்</h3>
+        <h1 className="animate-charcter">5,00,00,000</h1>
+      </div>
+    </div>
+  </div>
+  
+  )
+}
+
+export default Textu
